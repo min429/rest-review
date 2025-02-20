1751144960d08a613159f219e26873a4a1d5ed6b
min429 <lsm990429@gmail.com>
2025-02-20 14:48:08 +0900

    chore: 앱 기본 세팅

--- frontend/.vite/deps/react.js
+++ frontend/.vite/deps/react.js
@@ -1,4 +1,2 @@
-import {
-  require_react
-} from "./chunk-3ARHY7UZ.js";
+import { require_react } from "./chunk-3ARHY7UZ.js";
 export default require_react();
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "@/pages/LoginPage";
+import SignupPage from "@/pages/SignupPage";
+import QuestionListPage from "@/pages/QuestionListPage";
+import { AuthProvider } from "@/context/AuthContext";
+import Layout from "@/components/layout/Layout";
+import QuestionDetailPage from "@/pages/QuestionDetailPage";
+import QuestionCreatePage from "./pages/QuestionCreatePage";
+import QuestionEditPage from "./pages/QuestionEditPage";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <AuthProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<QuestionListPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/question/create" element={<QuestionCreatePage />} />
+            <Route path="/question/list" element={<QuestionListPage />} />
+            <Route path="/question/:id" element={<QuestionDetailPage />} />{" "}
+            <Route path="/question/:id/edit" element={<QuestionEditPage />} />{" "}
+          </Routes>
+        </Layout>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
