1fd338b55fcb8d6b7ee49a07c4d1eb1badf99789
min429 <lsm990429@gmail.com>
2025-02-20 14:51:35 +0900

    feat: auth 관련 context 추가

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, useState, ReactNode, useEffect } from "react";
+
+type AuthContextType = {
+  user: { email: string } | null;
+  setUser: (user: { email: string } | null) => void;
+};
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<{ email: string } | null>(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
